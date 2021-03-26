e91cec519255e8c222cd955c5d7899f7f2861e58
Shvets Kirill <kirill.shvets@intel.com>
2021-03-26 05:48:34 -0700

    debug tests

--- src/tree/updater_quantile_hist.cc
+++ src/tree/updater_quantile_hist.cc
@@ -646,7 +646,13 @@ bool QuantileHistMaker::Builder<GradientSumT>::UpdatePredictionCache(
   common::BlockedSpace2d space(n_nodes, [&](size_t node) {
     return row_set_collection_[node].Size();
   }, 1024);
-
+  std::cout << "nod_id: "<< std::endl;
+  size_t size = 0;
+  for(size_t i = 0; i < n_nodes; ++i) {
+    size += row_set_collection_[i].Size();
+  }
+  std::cout << "size: "<< size << std::endl;
+std::vector<int> node_ids(size);
   common::ParallelFor2d(space, this->nthread_, [&](size_t node, common::Range1d r) {
     const RowSetCollection::Elem rowset = row_set_collection_[node];
     if (rowset.begin != nullptr && rowset.end != nullptr) {
@@ -664,9 +670,14 @@ bool QuantileHistMaker::Builder<GradientSumT>::UpdatePredictionCache(
 
       for (const size_t* it = rowset.begin + r.begin(); it < rowset.begin + r.end(); ++it) {
         out_preds[*it] += leaf_value;
+        node_ids[*it] = nid;
       }
     }
   });
+for(size_t i = 0; i < node_ids.size(); ++i) {
+  std::cout << node_ids[i] << "  ";
+}
+  std::cout << std::endl;
 
   builder_monitor_.Stop("UpdatePredictionCache");
   return true;
