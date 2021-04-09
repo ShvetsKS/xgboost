5a79c1e861fce270a47ad92303f467bfafda31c3
Shvets Kirill <kirill.shvets@intel.com>
2021-04-09 07:12:31 -0700

    fix check

--- src/tree/updater_quantile_hist.cc
+++ src/tree/updater_quantile_hist.cc
@@ -1919,7 +1919,7 @@ static uint64_t n_call = 0;
   // }
   std::vector<size_t>& row_indices = *row_set_collection_.Data();
   const size_t size_threads = row_indices.size() == 0 ? (gmat.row_ptr.size() - 1) : row_indices.size();
-  if (param_.subsample < 1.0f) {
+  if (param_.subsample >= 1.0f) {
     CHECK_EQ(row_indices.size(), 0);
   }
   common::BlockedSpace2d space(1, [&](size_t node) {
